940e320badecaacd5aa79f6923ff36e39431b5bf
Kirsten Keels <keelsk@gmail.com>
2020-03-21 03:09:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,9 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+  
+  def self.most_popular_show
+    song = self.highest_rating
+    song.name
+  end
 end
